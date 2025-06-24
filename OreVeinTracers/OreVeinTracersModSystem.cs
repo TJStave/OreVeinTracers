06143cf3f68e07760d34e7c26eced3585573302c
TJStave <n01286513@humbermail.ca>
2025-06-24 18:13:08 -0400

    Made conditions for tracers less restrictive
    Meant to commit this earlier, whoops

--- OreVeinTracers/OreVeinTracersModSystem.cs
+++ OreVeinTracers/OreVeinTracersModSystem.cs
@@ -3,12 +3,14 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 using HarmonyLib;
+using System;
 
 namespace OreVeinTracers;
 
 public class OreVeinTracersModSystem : ModSystem
 {
     internal static Harmony harmony;
+    public static OreVeinTracersConfig config;
 
     public const string OreVeinTracersPatchCategory = "oreVeinTracers";
     // Called on server and client
@@ -16,6 +18,9 @@ public class OreVeinTracersModSystem : ModSystem
     public override void Start(ICoreAPI api)
     {
         base.Start(api);
+
+        TryToLoadConfig(api);
+
         if (harmony == null)
         {
             harmony = new Harmony(Mod.Info.ModID);
@@ -29,5 +34,28 @@ public class OreVeinTracersModSystem : ModSystem
         harmony?.UnpatchAll(Mod.Info.ModID);
         harmony = null;
         base.Dispose();
+    }
+
+    private void TryToLoadConfig(ICoreAPI api)
+    {
+        //It is important to surround the LoadModConfig function in a try-catch. 
+        //If loading the file goes wrong, then the 'catch' block is run.
+        try
+        {
+            config = api.LoadModConfig<OreVeinTracersConfig>("oreveintracersconfig.json");
+            if (config == null) //if the 'oreveintracersconfig.json' file isn't found...
+            {
+                config = new OreVeinTracersConfig();
+            }
+            //Save a copy of the mod config.
+            api.StoreModConfig<OreVeinTracersConfig>(config, "oreveintracersconfig.json");
+        }
+        catch (Exception e)
+        {
+            //Couldn't load the mod config... Create a new one with default settings, but don't save it.
+            Mod.Logger.Error("Could not load config! Loading default settings instead.");
+            Mod.Logger.Error(e);
+            config = new OreVeinTracersConfig();
+        }
     }
 }
--- OreVeinTracers/OreVeinTracersPatches.cs
+++ OreVeinTracers/OreVeinTracersPatches.cs
@@ -30,52 +30,77 @@ namespace OreVeinTracers
 
         private static bool GenerateTracer(IWorldAccessor world, BlockPos pos)
         {
-            Block brokenBlock = world.BlockAccessor.GetBlock(pos);
-            if (brokenBlock?.BlockMaterial != EnumBlockMaterial.Ore || (!brokenBlock?.Variant?.ContainsKey("type") ?? true))
+            Block brokenBlock = world.BlockAccessor.GetBlock(pos.Copy());
+            bool oreAround = false;
+
+            // first check if any ore is nearby
+            world.BlockAccessor.WalkBlocks(pos.AddCopy(-2, -2, -2), pos.AddCopy(2, 2, 2), (nblock, x, y, z) =>
             {
-                return true;
+                if (nblock?.BlockMaterial == EnumBlockMaterial.Ore && (nblock?.Variant?.ContainsKey("type") ?? false))
+                {
+                    oreAround = true;
+                }
+            });
+
+            // if no ore found, or broken block is not ore when config option onlyActivateWithinVein is enabled
+            if (!oreAround ||
+               (OreVeinTracersModSystem.config.onlyActivateWithinVein && (brokenBlock?.BlockMaterial != EnumBlockMaterial.Ore || (!brokenBlock?.Variant?.ContainsKey("type") ?? true))))
+            {
+                return true; // exit early
             }
+
+            // make a copy of pos so the value isn't messed up
             BlockPos decorPos = pos.Copy();
             foreach (BlockFacing facing in BlockFacing.ALLFACES)
             {
+                // iterate though surrounding blocks
                 facing.IterateThruFacingOffsets(decorPos);
 
+                // if block isn't stone, skip it
                 Block decorBlock = world.BlockAccessor.GetBlock(decorPos);
-
                 if (decorBlock?.FirstCodePart() != "rock" && decorBlock?.FirstCodePart() != "crackedrock")
                 {
                     continue;
                 }
 
+                // make another copy, this ones for the blocks surrounding the previous layer
                 BlockPos searchPos = decorPos.Copy();
                 bool oreFound = false;
                 foreach (BlockFacing searchFacing in BlockFacing.ALLFACES)
                 {
+                    // iterate through again
                     searchFacing.IterateThruFacingOffsets(searchPos);
+                    // skip checking the original block that was broken
                     if (searchFacing == facing.Opposite)
                     {
                         continue;
                     }
+                    // check if ore
                     Block checkBlock = world.BlockAccessor.GetBlock(searchPos);
-                    if (CheckVariant(checkBlock) == CheckVariant(brokenBlock))
+                    if (checkBlock?.BlockMaterial == EnumBlockMaterial.Ore && (checkBlock?.Variant?.ContainsKey("type") ?? false))
                     {
-                        Block tracerBlock = world.BlockAccessor.GetBlock("oreveintracers:oretracer-" + CheckVariant(brokenBlock)) ?? world.BlockAccessor.GetBlock("oreveintracers:placeholder");
-
+                        // add an overlay to the block based on the ore found
+                        // (edge case when more than 1 type of ore adjacent, will add the first type found, potentially misleading the player)
+                        Block tracerBlock = world.BlockAccessor.GetBlock("oreveintracers:oretracer-" + DecodeVariant(checkBlock)) ?? world.BlockAccessor.GetBlock("oreveintracers:placeholder");
                         world.BlockAccessor.SetDecor(tracerBlock, decorPos, facing.Opposite);
 
                         oreFound = true;
                         break;
                     }
                 }
+                // no ore found surrounding this block
                 if (!oreFound)
                 {
+                    //get all decors on the block
                     Dictionary<int, Block> decorsDict = world.BlockAccessor.GetSubDecors(decorPos);
                     if(decorsDict is null)
                     {
+                        // didn't find anything? then don't need to do anything
                         continue;
                     }
                     foreach (int key in decorsDict.Keys)
                     {
+                        // remove any tracer overlays, so the player knows there isn't any more ore around
                         if (decorsDict[key].FirstCodePart() == "oretracer")
                         {
                             world.BlockAccessor.SetDecor(world.BlockAccessor.GetBlock(0), decorPos, key);
@@ -88,7 +113,7 @@ namespace OreVeinTracers
         }
 
         // function to deal with sub-ores (gold and silver in quartz/galena and peridot in olivine)
-        public static string CheckVariant(Block block)
+        public static string DecodeVariant(Block block)
         {
             string oreType = block?.Variant["type"];
             if (oreType?.Contains('_') == true)
